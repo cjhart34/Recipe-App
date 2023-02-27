037dc6292a5d38cebfeb07fce5bd412d5aacae2b
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2023-02-27 17:23:22 -0600

    connected database to site and added styling

--- recipes/tests.py
+++ recipes/tests.py
@@ -23,4 +23,12 @@ class RecipeModelTest(TestCase):
     def test_cookingtime_helptext(self):
         recipe = Recipe.objects.get(id=1)
         recipe_cookingtime = recipe._meta.get_field('cooking_time').help_text
-        self.assertEqual(recipe_cookingtime, 'In minutes')
+        self.assertEqual(recipe_cookingtime, 'in minutes')
+
+    def test_get_absolute_url(self):
+        recipe = Recipe.objects.get(id=1)
+        self.assertEqual(recipe.get_absolute_url(), '/list/1')
+
+    def test_difficulty_calculation(self):
+        recipe = Recipe.objects.get(id=1)
+        self.assertEqual(recipe.calculate_difficulty(), 'Easy')
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Recipe
 
 # Create your views here.
 
 
 def welcome(request):
     return render(request, 'recipes/recipes_home.html')
+
+
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'recipes/main.html'
+
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipes/detail.html'
