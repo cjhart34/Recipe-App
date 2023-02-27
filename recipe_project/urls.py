b3eca6c35ca82cd25d6318f338a30cef58238bb2
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2023-02-27 10:29:10 -0600

    Created homepage

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -1,5 +1,4 @@
 """recipe_project URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
@@ -15,7 +14,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('recipes.urls'))
 ]
