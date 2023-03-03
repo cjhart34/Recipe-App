bcacb460198e13cec6e3841a0367cc1a9db5eecc
cjhart34 <cjhart34@gmail.com>
2023-03-02 19:31:36 -0600

    added user authentication

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -9,11 +10,11 @@ def welcome(request):
     return render(request, 'recipes/recipes_home.html')
 
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipes/main.html'
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipes/detail.html'
