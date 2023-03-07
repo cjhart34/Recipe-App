645ca54990bb2da7d0871254a6672f65c53ddb77
cjhart34 <cjhart34@gmail.com>
2023-03-07 17:48:11 -0600

    troubleshoot

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -11,6 +11,7 @@ import dj_database_url
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
