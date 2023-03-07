73f19251ad99ec788affdda28b2fe90f225134c7
cjhart34 <cjhart34@gmail.com>
2023-03-06 21:29:00 -0600

    Updated app with Django configurations

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -19,12 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--i$xy68z0vatselzw4#o6n43=-)&(dx2sv2bb)l6m&=z)%wem3'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'django-insecure-ml49cp(e)=yakpevh4xz)3w)6xuq6kv7g&3^xf^)gr-n3&p#%9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
