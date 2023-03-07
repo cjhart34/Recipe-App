dfd7f82019a2364d746c276d5d1b23fb9530d660
cjhart34 <cjhart34@gmail.com>
2023-03-07 13:34:17 -0600

    troubleshoot images not showing in database

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -135,7 +135,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_ROOT = BASE_DIR / 'media/recipes'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
